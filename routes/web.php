743f0aa60a7d12ad8865cefc1c88d07d56bbad91
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-03-21 02:05:59 +0700

    update 3 (main)

--- routes/web.php
+++ routes/web.php
@@ -28,29 +28,3 @@ Route::get('/admin/register-submit', [AdminController::class, 'register_submit']
 
 /* Marketing Manager */
 Route::get('/marketing-manager/login', [MarketingManagerController::class, 'login_view'])->name('manager_login'); // login
-
-
-//test
-Route::get('/home', function () {
-    return view('admin.home');
-});
-
-Route::get('/manage', function () {
-    return view('admin.action.index');
-});
-Route::get('/add', function () {
-    return view('admin.action.add');
-});
-Route::get('/edit', function () {
-    return view('admin.action..edit');
-});
-
-Route::get('/coordinatorLogin', function () {
-    return view('coordinator/auth.login');
-});
-Route::get('/marketingManagerLogin', function () {
-    return view('marketingManager/auth.login');
-});
-Route::get('/studentLogin', function () {
-    return view('student/auth.login');
-});
